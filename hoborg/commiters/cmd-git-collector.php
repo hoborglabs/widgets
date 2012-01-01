b4bd78573a738fffd633b33364b735628503fb05
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2012-01-01 22:27:16 +0100

    Wrapping CLI commands with classes.

--- hoborg/commiters/cmd-git-collector.php
+++ hoborg/commiters/cmd-git-collector.php
@@ -1,57 +1,64 @@
 <?php
-$defaults = array(
-	'git' => 'git',
-	'branch' => 'master',
-	'wd' => null,
-	'out' => null
-);
-$options = $params + $defaults;
-if (empty($options['wd']) || empty($options['out'])) {
-	exit('please specify Working Directory `wd` and Output Direcotry `out`');
-}
+namespace Hoborg\Dashboard\Widget\Commiters;
 
-$cmd = "{$options['git']} log -100 --format=\"%h,%ae,%an\" origin/{$options['branch']}";
-$logs = array();
+class GitCollector {
 
-chdir($options['wd']);
-exec($cmd, $logs);
-
-$commits = parse_log($logs);
-$commiters = get_commiters($commits);
-
-file_put_contents($options['out'], json_encode($commiters));
-
-function parse_log(array $logLines) {
-	$commits = array();
-
-	foreach ($logLines as $line) {
-		list($commitHash, $email, $name) = explode(',', $line, 3);
-		$commits[$commitHash] = array(
-			'name' => $name,
-			'email' => $email,
-			'commit' => $commitHash,
+	public function execute($params) {
+		$defaults = array(
+			'git' => 'git',
+			'branch' => 'master',
+			'wd' => null,
+			'out' => null
 		);
+		$options = $params + $defaults;
+		if (empty($options['wd']) || empty($options['out'])) {
+			exit("please specify Working Directory `wd` and Output Direcotry `out`\n");
+		}
+
+		$cmd = "{$options['git']} log -100 --format=\"%h,%ae,%an\" origin/{$options['branch']}";
+		$logs = array();
+
+		chdir($options['wd']);
+		exec($cmd, $logs);
+
+		$commits = $this->parse_log($logs);
+		$commiters = $this->get_commiters($commits);
+
+		file_put_contents($options['out'], json_encode($commiters));
 	}
 
-	return $commits;
-}
+	private function parse_log(array $logLines) {
+		$commits = array();
 
-function get_commiters(array $commits) {
-	$commiters = array();
-
-	foreach ($commits as $commit) {
-		if (!isset($commiters[$commit['email']])) {
-			$commiters[$commit['email']] = array(
-				'email' => $commit['email'],
-				'name' => $commit['name'],
-				'count' => 0,
+		foreach ($logLines as $line) {
+			list($commitHash, $email, $name) = explode(',', $line, 3);
+			$commits[$commitHash] = array(
+				'name' => $name,
+				'email' => $email,
+				'commit' => $commitHash,
 			);
 		}
 
-		$commiters[$commit['email']]['count']++;
+		return $commits;
 	}
 
-	uasort($commiters, function($a, $b) {return $b['count'] - $a['count']; });
+	private function get_commiters(array $commits) {
+		$commiters = array();
 
-	return $commiters;
+		foreach ($commits as $commit) {
+			if (!isset($commiters[$commit['email']])) {
+				$commiters[$commit['email']] = array(
+					'email' => $commit['email'],
+					'name' => $commit['name'],
+					'count' => 0,
+				);
+			}
+
+			$commiters[$commit['email']]['count']++;
+		}
+
+		uasort($commiters, function($a, $b) {return $b['count'] - $a['count']; });
+
+		return $commiters;
+	}
 }
--- hoborg/jenkins/cmd-collector.php
+++ hoborg/jenkins/cmd-collector.php
@@ -1,49 +1,56 @@
 <?php
-$defaults = array(
-	'url' => null,
-	'out' => null
-);
-$options = $params + $defaults;
-if (empty($options['url']) || empty($options['out'])) {
-	exit('please specify Jenkins URL `url` and Output File `out`');
-}
+namespace Hoborg\Dashboard\Widget\Jenkins;
 
-$tree = array(
-    'jobs' => array(
-        'name',
-        'id',
-        'color',
-        'inQueue',
-        'lastBuild' => array(
-            'number',
-            'timestamp',
-            'result',
-            'url',
-            'building',
-        ),
-        'healthReport' => array('score', 'description'),
-    )
-);
-$url = $options['url'] . '/api/json?tree=';
-$url .= urlencode(get_tree_value($tree));
+class Collector {
 
-// get data from url
-$data = file_get_contents($url);
-if (empty($data)) {
-    die('no Data returned from ' . $url);
-}
-file_put_contents($options['out'], $data);
+	public function execute($params) {
+	$defaults = array(
+		'url' => null,
+		'out' => null
+	);
+	$options = $params + $defaults;
+	if (empty($options['url']) || empty($options['out'])) {
+		exit("please specify Jenkins URL `url` and Output File `out`\n");
+	}
 
-function get_tree_value(array $tree) {
-    $value = '';
+	$tree = array(
+	    'jobs' => array(
+	        'name',
+	        'id',
+	        'color',
+	        'inQueue',
+	        'lastBuild' => array(
+	            'number',
+	            'timestamp',
+	            'result',
+	            'url',
+	            'building',
+	        ),
+	        'healthReport' => array('score', 'description'),
+	    )
+	);
+	$url = $options['url'] . '/api/json?tree=';
+	$url .= urlencode(get_tree_value($tree));
 
-    foreach ($tree as $key => $val) {
-        if (is_array($val)) {
-            $value .= ','.$key.'['.get_tree_value($val).']';
-        } else {
-            $value .= ','.$val;
-        }
-    }
+	// get data from url
+	$data = file_get_contents($url);
+	if (empty($data)) {
+	    die('no Data returned from ' . $url);
+	}
+	file_put_contents($options['out'], $data);
 
-    return substr($value, 1);
+	function get_tree_value(array $tree) {
+	    $value = '';
+
+	    foreach ($tree as $key => $val) {
+	        if (is_array($val)) {
+	            $value .= ','.$key.'['.get_tree_value($val).']';
+	        } else {
+	            $value .= ','.$val;
+	        }
+	    }
+
+	    return substr($value, 1);
+	}
+	}
 }
