a22deb19fcb2b080eeaf6d1774fa501ffb2b5efa
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2012-01-11 21:20:34 +0000

    Quick migration to widget Classes

--- hoborg/jenkins/widget.php
+++ hoborg/jenkins/widget.php
@@ -1,23 +1,31 @@
 <?php
-// main check for data file.
-if (empty($widget['conf']['data'])) {
-	$widget['body'] = '';
-	$widget['error'] = 'Missing or empty data configuration `widget.conf.data`';
-	return $widget;
+
+class JenkinsWidget extends \Hoborg\Dashboard\Widget {
+
+	public function bootstrap() {
+		$widget = $this->data;
+
+		// main check for data file.
+		if (empty($widget['conf']['data'])) {
+			$widget['body'] = '';
+			$widget['error'] = 'Missing or empty data configuration `widget.conf.data`';
+			return $widget;
+		}
+
+		$dataFile = $path = $this->kernel->findFileOnPath(
+			$widget['conf']['data'],
+			$this->kernel->getDataPath()
+		);
+
+
+		$jobs = json_decode(file_get_contents($dataFile), true);
+
+		$jobs = $jobs['jobs'];
+
+		ob_start();
+		include __DIR__ . '/view.phtml';
+		$widget['body'] = ob_get_clean();
+
+		$this->data = $widget;
+	}
 }
-
-$dataFile = $path = $this->kernel->findFileOnPath(
-	$widget['conf']['data'],
-	$this->kernel->getDataPath()
-);
-
-
-$jobs = json_decode(file_get_contents($dataFile), true);
-
-$jobs = $jobs['jobs'];
-
-ob_start();
-include __DIR__ . '/view.phtml';
-$widget['body'] = ob_get_clean();
-
-return $widget;
