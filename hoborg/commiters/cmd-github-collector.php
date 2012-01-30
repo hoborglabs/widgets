a0d2fd73ace1225b8fc7bcc14e4cdb0bf07a59be
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2012-01-30 18:24:06 +0000

    Adding github commiters collector

---
+++ hoborg/commiters/cmd-github-collector.php
@@ -0,0 +1,79 @@
+<?php
+namespace Hoborg\Dashboard\Widget\Commiters;
+
+class GithubCollector {
+
+	public function execute($params) {
+		$defaults = array(
+			'user' => null,
+			'repo' => null,
+			'branch' => 'master',
+			'out' => null,
+		);
+		$options = $params + $defaults;
+		if (empty($options['user']) || empty($options['repo']) || empty($options['out'])) {
+			$this->printHelp();
+		}
+		$url = "https://api.github.com/repos/{$options['user']}/{$options['repo']}/commits?sha={$options['branch']}";
+		$logs = file_get_contents($url);
+
+		if (empty($logs)) {
+			exit('No logs returned');
+		}
+
+		$commits = json_decode($logs, true);
+
+		$commits = $this->parseCommits($commits);
+		$commiters = $this->getCommiters($commits);
+
+		file_put_contents($options['out'], json_encode($commiters));
+	}
+
+	private function parseCommits(array $githubCommits) {
+		$commits = array();
+
+		foreach ($githubCommits as $commit) {
+			$commits[$commit['sha']] = array(
+				'name' => $commit['commit']['author']['name'],
+				'email' => $commit['commit']['author']['email'],
+				'commit' => $commit['sha'],
+			);
+		}
+
+		return $commits;
+	}
+
+	private function getCommiters(array $commits) {
+		$commiters = array();
+
+		foreach ($commits as $commit) {
+			if (!isset($commiters[$commit['email']])) {
+				$commiters[$commit['email']] = array(
+					'email' => $commit['email'],
+					'name' => $commit['name'],
+					'count' => 0,
+				);
+			}
+
+			$commiters[$commit['email']]['count']++;
+		}
+
+		uasort($commiters, function($a, $b) {return $b['count'] - $a['count']; });
+
+		return $commiters;
+	}
+
+	private function printHelp() {
+		$help = "
+please specify options for Github collector:
+  user    github user
+  repo    github repository name
+  branch  (optional) branch name
+  out     output file name
+
+example usage
+./bin/cmd -c widget.hoborg.commiters.github-collector -d user=hoborglabs -d repo=Dashboard -d out=commits.json
+";
+		exit($help);
+	}
+}
